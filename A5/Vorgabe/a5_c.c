27d6cbbdd8d4ae26e89072dbf4ab211c175202c7
illfixit <66363651+illfixit@users.noreply.github.com>
2024-06-30 16:45:15 +0200

    First commit

---
+++ A5/Vorgabe/a5_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int breite;
+int hoehe;
+long dateigroesse;
+
+int main( int argc, char *argv[] )  {
+
+	printf("Breite: %d \nHoehe: %d \nDateigroesse: %ld bytes\n",breite,hoehe,dateigroesse);
+	return 0;
+	
+}
