b63c9304e142c1af48799586b8c7a11f051385ab
Ilia Orlov <66363651+illfixit@users.noreply.github.com>
2024-06-30 15:15:05 +0000

    add a

--- A5/Vorgabe/a5_a.c
+++ A5/Vorgabe/a5_a.c
@@ -6,6 +6,29 @@ long dateigroesse;
 
 int main( int argc, char *argv[] )  {
 
+	// fopen(3), fclose(3), fseek(3), ftell(3), fscanf(3) und fprintf(3)
+
+	if(argc != 2) {
+		printf("Falsche Anzahl an Argumenten\n");
+		return 1;
+	}
+
+	FILE *bild = fopen(argv[1],"r");
+
+	if(bild == NULL) {
+		printf("Fehler: Datei konnte nicht geöffnet werden\n");
+		return 1;
+	}
+
+	if(fscanf(bild,"P3%d %d",&breite,&hoehe) != 2) {
+		printf("Fehler: Datei konnte nicht gelesen werden\n");
+		return 1;
+	}
+
+	fseek(bild, 0, SEEK_END); 
+	long dateigroesse = ftell(bild); 
+	fseek(bild, 0, SEEK_SET);
+
 	printf("Breite: %d \nHoehe: %d \nDateigroesse: %ld bytes\n",breite,hoehe,dateigroesse);
 	return 0;
 	
